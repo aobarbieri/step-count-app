e811cf4245d7b1ced82a8b489e8a850b04c9c862
aobarbieri <aobarbieri@gmail.com>
2024-04-16 12:56:48 -0400

    Initial Commit

---
+++ Step Count Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Step Count Watch App
+//
+//  Created by Amanda Mitzian on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Step Count Watch App/Step_CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_CountApp.swift
+//  Step Count Watch App
+//
+//  Created by Amanda Mitzian on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_Count_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
